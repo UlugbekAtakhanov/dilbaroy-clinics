929500ca7f8e3b063a17f32f2d6eca3098b39bf6
Ulugbek Atakhanov <atakhanov_ux@mail.ru>
2023-06-06 21:53:11 +0500

    May6 21:53

--- src/axios/global-instances.tsx
+++ src/axios/global-instances.tsx
@@ -6,8 +6,8 @@ import { getFromLS } from "../utils/localStorage";
 axios.defaults.headers.post["Content-Type"] = 'application/json'
 
 export const fetchData = axios.create({
-    baseURL: "https://dilbaroy.pythonanywhere.com/",
-    // baseURL: "https://klinika.pythonanywhere.com",
+    // baseURL: "https://dilbaroy.pythonanywhere.com/",
+    baseURL: "http://85.92.110.9/",
 })
 
 
